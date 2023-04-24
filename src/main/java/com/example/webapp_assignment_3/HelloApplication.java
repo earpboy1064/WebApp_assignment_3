0f737404433202eb570ef0527201599af3bb7954
earpboy1064 <31712696+earpboy1064@users.noreply.github.com>
2023-04-24 13:39:42 -0500

    first commit

---
+++ src/main/java/com/example/webapp_assignment_3/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.webapp_assignment_3;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
